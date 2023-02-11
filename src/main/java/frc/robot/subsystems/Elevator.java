0349041d938171bc96b36a69727634573dbbaceb
Kevin-OConnor <koconnor@usfirst.org>
2023-02-11 01:15:46 -0500

    Add Elevator pose logging

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -10,19 +10,27 @@ import com.revrobotics.CANSparkMax.SoftLimitDirection;
 import com.revrobotics.SparkMaxLimitSwitch.Type;
 
 import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.core238.PoseHelper;
 import frc.robot.RobotMap;
 import frc.robot.RobotMap.ElevatorParameters;
-
 public class Elevator extends SubsystemBase {
   protected final static ElevatorFeedforward FF = new ElevatorFeedforward(ElevatorParameters.ks, ElevatorParameters.kg, ElevatorParameters.kv);
   protected final static CANSparkMax elevatorLeader = RobotMap.ElevatorParameters.elevatorLeader;
   protected final static CANSparkMax elevatorFollower = RobotMap.ElevatorParameters.elevatorFollower;
+  protected Pose3d elevatorPose = new Pose3d();
+  protected GenericEntry poseEntry;
   
   /** Creates a new Elevator. */
   public Elevator() {
     initSparkMax();
+    poseEntry = Shuffleboard.getTab("Logging").add("ElevatorPose", PoseHelper.PoseToArray(elevatorPose)).getEntry();
   }
   
   public void initSparkMax() {
@@ -45,13 +53,22 @@ public class Elevator extends SubsystemBase {
     return elevatorLeader.getEncoder().getPosition();
   }
 
+  public double getPositionMeters() {
+    //1.5 inches per pulley rotation divided by 42 ppr and 4:1 gear ratio
+    return Units.inchesToMeters(1.5*getEncoderPosition()/(42.0*4));
+  }
+
+  public Pose3d getPose() {
+    return new Pose3d(getPositionMeters()*Math.sin(Math.toRadians(35)), 0, getPositionMeters()*Math.cos(Math.toRadians(35)), new Rotation3d());
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    if(elevatorLeader.getReverseLimitSwitch(Type.kNormallyOpen).get()) {
+    if(elevatorLeader.getReverseLimitSwitch(Type.kNormallyOpen).isPressed()) {
      elevatorLeader.getEncoder().setPosition(0);
-    
     }
+    poseEntry.setDoubleArray(PoseHelper.PoseToArray(getPose()));
   }
 
   public void PIDdrive(TrapezoidProfile.State state) {
