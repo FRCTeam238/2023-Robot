fa95ffdbef65aa111d08584da113d2e73fbf273b
Kevin-OConnor <koconnor@usfirst.org>
2023-02-11 01:15:34 -0500

    Add Drivetrain Simulation and Field2D pose logging

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -5,17 +5,31 @@
 package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.DemandType;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.TalonFXSimCollection;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.sensors.SensorVelocityMeasPeriod;
 import com.kauailabs.navx.frc.AHRS;
 
-import edu.wpi.first.math.controller.DifferentialDriveFeedforward;
+import edu.wpi.first.hal.SimDouble;
+import edu.wpi.first.hal.simulation.SimDeviceDataJNI;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.simulation.BatterySim;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
+import edu.wpi.first.wpilibj.simulation.RoboRioSim;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
@@ -37,15 +51,32 @@ public class Drivetrain extends SubsystemBase {
 
   public static SimpleMotorFeedforward feedForward = new SimpleMotorFeedforward(RobotMap.DrivetrainParameters.kS, RobotMap.DrivetrainParameters.kV, RobotMap.DrivetrainParameters.kA);
 
+  protected Field2d robotPose;
   
   Pose2d currentPose;
   AHRS navx;
 
+  DifferentialDrivetrainSim m_driveSim;
+  TalonFXSimCollection m_leftDriveSim = leftControllerDrive.getSimCollection();
+  TalonFXSimCollection m_rightDriveSim = rightControllerDrive.getSimCollection();
+
   /** Creates a new Drivetrain. */
   public Drivetrain() {
-    initTalons();
     navx = new AHRS(RobotMap.VisionParameters.navxPort);
+    differentialDriveOdometry = new DifferentialDriveOdometry(navx.getRotation2d(), getLeftEncoderTicks(), getRightEncoderTicks());
+    initTalons();
     
+    robotPose = new Field2d();
+    Shuffleboard.getTab("Logging").add("RobotPose", robotPose);
+
+    m_driveSim = new DifferentialDrivetrainSim(
+      // Create a linear system from our identification gains.
+      LinearSystemId.identifyDrivetrainSystem(RobotMap.DrivetrainParameters.kVLinear, RobotMap.DrivetrainParameters.kALinear, RobotMap.DrivetrainParameters.kVAngular, RobotMap.DrivetrainParameters.kAAngular),
+      DCMotor.getFalcon500(2),       // 2 Falcon motors on each side of the drivetrain.
+      10.86,                    // 10.86:1 gearing reduction.
+      RobotMap.DrivetrainParameters.trackWidth,                  
+      Units.inchesToMeters(RobotMap.DrivetrainParameters.wheelDiameterInches/2),
+      null);
   }
 
   public void initTalons() {
@@ -63,12 +94,7 @@ public class Drivetrain extends SubsystemBase {
     leftControllerDrive.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, RobotMap.DrivetrainParameters.currentLimit, RobotMap.DrivetrainParameters.triggerThresholdCurrent, 0.5));
     rightFollower.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, RobotMap.DrivetrainParameters.currentLimit, RobotMap.DrivetrainParameters.triggerThresholdCurrent, 0.5));
     leftFollower.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, RobotMap.DrivetrainParameters.currentLimit, RobotMap.DrivetrainParameters.triggerThresholdCurrent, 0.5));
-    differentialDriveOdometry = new DifferentialDriveOdometry(null, getLeftEncoderTicks(), getRightEncoderTicks());
-    
-    
 
-    
-  }
     //Set the encoder status frame to update every 1ms to minimize latency and jitter
     initPID(leftControllerDrive);
     initPID(rightControllerDrive);
@@ -113,8 +139,9 @@ public class Drivetrain extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    differentialDriveOdometry.update(navx.getRotation2d(), getLeftEncoderTicks(), getRightEncoderTicks());
+    differentialDriveOdometry.update(navx.getRotation2d(), stepsToMeters(getLeftEncoderTicks()), stepsToMeters(getRightEncoderTicks()));
     currentPose = differentialDriveOdometry.getPoseMeters();
+    robotPose.setRobotPose(currentPose);
   }
 
   public void resetOdometry(Pose2d pose) {
@@ -142,9 +169,7 @@ public class Drivetrain extends SubsystemBase {
 
 
   public void tankDrive(double left, double right) {
-
     diff.tankDrive(left, right, false);
-
   }
 
   public void arcadeDrive(double left, double right) {
@@ -177,7 +202,7 @@ public class Drivetrain extends SubsystemBase {
     return stepsToMeters(stepsPerDecisec * 10.0);
   }
 
-  public static double metersPerDecisecToStepsToSec(double metersPerSec) {
+  public static double metersPerSecToStepsPerDecisec(double metersPerSec) {
     return metersToSteps(metersPerSec) / 10.0;
   }
 
@@ -191,6 +216,32 @@ public class Drivetrain extends SubsystemBase {
 
   public static double insPerSecToStepsToDecisec(double inchesPerSecond) {
     return insToSteps(inchesPerSecond) * 0.1;
+  }
+
+  @Override
+  public void simulationPeriodic()
+  {
+    m_leftDriveSim.setBusVoltage(12);
+    m_rightDriveSim.setBusVoltage(12);
+    m_driveSim.setInputs(-m_leftDriveSim.getMotorOutputLeadVoltage(),
+                         m_rightDriveSim.getMotorOutputLeadVoltage());
+
+    // Advance the model by 20 ms. Note that if you are running this
+    // subsystem in a separate thread or have changed the nominal timestep
+    // of TimedRobot, this value needs to match it.
+    m_driveSim.update(0.02);
+
+    System.out.println("Left, Right " + m_driveSim.getLeftPositionMeters() + "," + m_driveSim.getRightPositionMeters()); 
+
+    // Update all of our sensors.
+    leftControllerDrive.getSimCollection().setIntegratedSensorRawPosition((int) metersToSteps(m_driveSim.getLeftPositionMeters()));
+    leftControllerDrive.getSimCollection().setIntegratedSensorVelocity((int) metersPerSecToStepsPerDecisec(m_driveSim.getLeftVelocityMetersPerSecond()));
+    rightControllerDrive.getSimCollection().setIntegratedSensorRawPosition(-(int) metersToSteps(m_driveSim.getRightPositionMeters()));
+    rightControllerDrive.getSimCollection().setIntegratedSensorVelocity(-(int) metersPerSecToStepsPerDecisec(m_driveSim.getRightVelocityMetersPerSecond()));    
+    int dev = SimDeviceDataJNI.getSimDeviceHandle("navX-Sensor[0]");
+    SimDouble angle = new SimDouble(SimDeviceDataJNI.getSimValueHandle(dev, "Yaw"));
+    angle.set(-m_driveSim.getHeading().getDegrees());
+    RoboRioSim.setVInVoltage(BatterySim.calculateDefaultBatteryLoadedVoltage(m_driveSim.getCurrentDrawAmps()));
   }
 
 }
