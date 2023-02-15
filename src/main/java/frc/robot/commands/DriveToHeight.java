5f323b1aa80d4235ce31d93314940e564dcd7d8b
Kevin-OConnor <koconnor@usfirst.org>
2023-02-15 12:20:23 -0500

    Fix DriveToHeight to not override important lifecycle methods

--- src/main/java/frc/robot/commands/DriveToHeight.java
+++ src/main/java/frc/robot/commands/DriveToHeight.java
@@ -6,7 +6,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.TrapezoidProfileCommand;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
@@ -18,23 +17,5 @@ public class DriveToHeight extends TrapezoidProfileCommand {
     super(new TrapezoidProfile(new TrapezoidProfile.Constraints(RobotMap.ElevatorParameters.MaxVel, RobotMap.ElevatorParameters.MaxAccel), endState, 
     new TrapezoidProfile.State(Robot.elevator.getEncoderPosition(),Robot.elevator.getVelocity())), 
      setpointState -> Robot.elevator.PIDdrive(setpointState), Robot.elevator);
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
   }
 }
