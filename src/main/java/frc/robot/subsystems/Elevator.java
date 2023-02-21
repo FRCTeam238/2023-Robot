03e71eb4f22da25ac9bf5b820492e41487a7f982
Kevin-OConnor <koconnor@usfirst.org>
2023-02-21 15:00:05 -0500

    Add elevator simulation

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -9,16 +9,25 @@ import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMax.SoftLimitDirection;
 import com.revrobotics.SparkMaxLimitSwitch.Type;
 
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.simulation.BatterySim;
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
+import edu.wpi.first.wpilibj.simulation.PWMSim;
+import edu.wpi.first.wpilibj.simulation.RoboRioSim;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.core238.PoseHelper;
+import frc.robot.Robot;
 import frc.robot.RobotMap;
 import frc.robot.RobotMap.ElevatorParameters;
 public class Elevator extends SubsystemBase {
@@ -27,6 +36,11 @@ public class Elevator extends SubsystemBase {
   protected final static CANSparkMax elevatorFollower = RobotMap.ElevatorParameters.elevatorFollower;
   protected Pose3d elevatorPose = new Pose3d();
   protected GenericEntry poseEntry;
+
+  private final DCMotor m_simMotors = DCMotor.getNEO(2);
+  private final ElevatorSim m_sim = new ElevatorSim(m_simMotors, 4, Units.lbsToKilograms(10), Units.inchesToMeters(.75), 0, Units.inchesToMeters(36), true, VecBuilder.fill(0));
+  private final PWMSparkMax m_simMotor = new PWMSparkMax(0);
+  private final PWMSim m_motorSim = new PWMSim(m_simMotor);
   
   /** Creates a new Elevator. */
   public Elevator() {
@@ -49,11 +63,20 @@ public class Elevator extends SubsystemBase {
   }
 
   public void moveByPercentOutput(double percent) {
-    elevatorLeader.set(percent);
+    if(Robot.isReal()){
+      elevatorLeader.set(percent);
+    } else {
+      m_simMotor.set(percent);
+    }
   }
 
   public double getEncoderPosition() {
-    return elevatorLeader.getEncoder().getPosition();
+    if(Robot.isReal()) {
+      return elevatorLeader.getEncoder().getPosition();
+    } else
+    {
+      return RobotMap.ElevatorParameters.inchesToTicks(Units.metersToInches(m_sim.getPositionMeters()));
+    }
   }
 
   public double getPositionMeters() {
@@ -82,15 +105,41 @@ public class Elevator extends SubsystemBase {
     SmartDashboard.putNumber("Actual Pos", getEncoderPosition());
     double feed = FF.calculate(state.velocity);
     SmartDashboard.putNumber("FF", feed);
+    if(Robot.isReal()){
     elevatorLeader.getPIDController().setReference(state.position, ControlType.kPosition, 0, feed);
+    }
+    else {
+      feed = feed + 1*(state.position - getEncoderPosition());
+      m_simMotor.setVoltage(feed);
+    }
 
   }
 
   public double getVelocity() {
+    if(Robot.isReal())
+    {
     return elevatorLeader.getEncoder().getVelocity();
+    }
+    else {
+      return RobotMap.ElevatorParameters.inchesToTicks(Units.metersToInches(m_sim.getVelocityMetersPerSecond()));
+    }
   }
 
   public boolean getLowerLimit() {
     return elevatorLeader.getReverseLimitSwitch(Type.kNormallyOpen).isPressed();
+  }
+
+  public void simulationPeriodic() {
+    // In this method, we update our simulation of what our elevator is doing
+    // First, we set our "inputs" (voltages)
+    m_sim.setInput(m_motorSim.getSpeed() * RobotController.getBatteryVoltage());
+
+    // Next, we update it. The standard loop time is 20ms.
+    m_sim.update(0.020);
+
+    // Finally, we set our simulated encoder's readings and simulated battery voltage
+    // SimBattery estimates loaded battery voltages
+    RoboRioSim.setVInVoltage(
+        BatterySim.calculateDefaultBatteryLoadedVoltage(m_sim.getCurrentDrawAmps()));
   }
 }
