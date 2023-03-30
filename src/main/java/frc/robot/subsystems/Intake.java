51c156587289455ecfeb0f9e614b5ebf17c47a11
Kevin-OConnor <koconnor@usfirst.org>
2023-03-29 20:38:03 -0400

    Add logging to intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,43 +5,37 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.util.datalog.BooleanLogEntry;
-import edu.wpi.first.util.datalog.DoubleArrayLogEntry;
 import edu.wpi.first.util.datalog.DoubleLogEntry;
 import edu.wpi.first.util.datalog.StringLogEntry;
 import edu.wpi.first.wpilibj.DataLogManager;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
 public class Intake extends SubsystemBase {
 
+  protected DoubleLogEntry logIntake;
+  protected StringLogEntry logCommand;
 
   /** Creates a new Intake. */
   public Intake() {
-
+    logCommand = new StringLogEntry(DataLogManager.getLog(), "Intake:Command");
+    logIntake = new DoubleLogEntry(DataLogManager.getLog(), "Intake:Speed");
   }
 
   public void run (double intakeSpeed) {
     RobotMap.IntakeParameters.intakeMotor.set(ControlMode.PercentOutput, intakeSpeed);
-  
+    logIntake.append(intakeSpeed);
   }
 
   public void stop () {
     RobotMap.IntakeParameters.intakeMotor.set(ControlMode.PercentOutput, 0);
-    
+    logIntake.append(0);
   }
 
+  public void putCommandString(Command command) {
+      logCommand.append(command.getName());
+  }
 
   @Override
   public void periodic() {
