d7b0ac538f5d974515b0c087663ecb1d6f824d4d
Kevin-OConnor <koconnor@usfirst.org>
2023-04-03 13:32:33 -0400

    Make Release Gamepiece an auto command

--- src/main/java/frc/robot/commands/ReleaseGamepieceCommand.java
+++ src/main/java/frc/robot/commands/ReleaseGamepieceCommand.java
@@ -4,20 +4,37 @@
 
 package frc.robot.commands;
 
+import java.util.List;
+
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.core238.autonomous.AutonomousModeAnnotation;
 import frc.robot.Robot;
 import frc.robot.subsystems.Intake;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class ReleaseGamepieceCommand extends SequentialCommandGroup {
+
+@AutonomousModeAnnotation(parameterNames = {})
+public class ReleaseGamepieceCommand extends SequentialCommandGroup implements IAutonomousCommand {
   /** Creates a new ReleaseGamepieceCommand. */
   public ReleaseGamepieceCommand() {
     // Add your commands in the addCommands() call, e.g.
     Intake intake = Robot.intake;
     addRequirements(intake);
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new IntakeInOutCommand(IntakeInOutCommand.Direction.Out).withTimeout(2));
+    addCommands(new IntakeInOutCommand(IntakeInOutCommand.Direction.Out).withTimeout(.5));
+  }
+
+  @Override
+  public void setParameters(List<String> parameters) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public double getTimeout() {
+    // TODO Auto-generated method stub
+    return 0;
   }
 }
