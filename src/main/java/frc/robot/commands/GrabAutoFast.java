a484c5fa3f3bf93d1d6ccac926c8639049ad1e9c
Kevin-OConnor <koconnor@usfirst.org>
2023-04-03 14:51:23 -0400

    Add a grab auto that returns no matter what for charge

---
+++ src/main/java/frc/robot/commands/GrabAutoFast.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.core238.autonomous.AutonomousModeAnnotation;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+@AutonomousModeAnnotation(parameterNames = {})
+public class GrabAutoFast extends SequentialCommandGroup implements IAutonomousCommand {
+  /** Creates a new GrabAuto. */
+  List<String> params = new ArrayList<String>();
+
+  public GrabAutoFast() {
+    addCommands(new FloorTippedHeight());
+    addCommands(new IntakeInOutCommand(IntakeInOutCommand.Direction.In).alongWith(new DriveStraightInches(18, .165)));
+  }
+  
+  @Override
+  public void setParameters(List<String> parameters) {
+
+  }
+
+  @Override
+  public double getTimeout() {
+    return 0;
+  }
+}
